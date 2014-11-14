4e35b8fd2042001210adc08c9e4d676f75668fc7
Noel Hidalgo <nhidalgoca@gmail.com>
2014-11-14 15:23:51 -0600

    Version con tabla de datos en tiempo real

--- js/control.js
+++ js/control.js
@@ -26,6 +26,59 @@
                                     success: function(info){  //funcion que se ejecutara cuando el servidor de una respuesta
                                         console.log(info); //info son los datos que devuelve el servidor
                                         response = info;
+                                        if(response>=80 && response<=90)
+                                        {
+                                            var nodo=2;
+                                            var longitud=31.2;
+                                            var distancia=7.5;
+                                            var velocidad=longitud*response;
+
+                                        }
+                                        if(response>=112 && response<=135)
+                                        {
+                                            var nodo=3;
+                                            var longitud= 21.7;
+                                            var distancia=3.0;
+                                             var velocidad=longitud*response;
+
+                                        }
+                                        if(response>=155 && response<=177)
+                                        {
+                                            var nodo=4;
+                                            var longitud= 16.13;
+                                            var distancia=4.5;
+                                             var velocidad=longitud*response;
+                                        }
+                                        if(response>=198 && response<=213)
+                                        {
+                                            var nodo=5;
+                                            var longitud= 15.7;
+                                            var distancia=3.8;
+                                             var velocidad=longitud*response;
+
+                                        }
+                                        if(response>=240 && response<=264)
+                                        {
+                                            var nodo=6;
+                                            var longitud=10.7;
+                                            var distancia=4.3; 
+                                            var velocidad=longitud*response;                                           
+
+                                        }
+                                        var capa = document.getElementById("nodos").innerHTML=nodo;
+                                        //capa.innerHTML = nodo;
+                                        var capa2 = document.getElementById("longitud");
+                                        capa2.innerHTML = longitud;
+                                        var capa = document.getElementById("distancia");
+                                        capa.innerHTML = distancia;
+                                        var capa = document.getElementById("periodo");
+                                        capa.innerHTML = 1/response;
+                                        var capa = document.getElementById("frecuencia");
+                                        capa.innerHTML = response;
+                                        var capa = document.getElementById("velocidad");
+                                        capa.innerHTML =  velocidad;
+
+                                        //$('#simulacion').attr("src", "imagenes/4Nodos.gif");
                                     },
                                     error: function(jqXHR, estado, error){ //en caso de error, se ejecuta esto 
                                         console.log(error);
@@ -94,10 +147,7 @@ $(document).on('ready', function(){
     $('#range').on('change', function(){
         valorRange = $(this).val();
         $(rangeValue).val(valorRange);
-        if(valorRange>=100 && valorRange<=300)
-        {
-            $('#simulacion').attr("src", "imagenes/6Nodos.gif");
-        }
+       
         /**************  Comunicacion con el server por ajax  **************/
             $.ajax({
                 url: 'conexion/controller.php',
